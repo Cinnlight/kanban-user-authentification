de963ad657cb7b8156b46db75c1392f1dd467e67
Cinnlight <cinnlight@gmail.com>
2024-11-21 14:22:59 -0700

    Add logic to client-sided login POST request

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -2,6 +2,32 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    if (!response.ok) {
+      const errorMessage = await response.json();
+      throw new Error(errorMessage.message || 'Failed to log in');
+    }
+
+    const data = await response.json();
+    console.log(data);
+
+    if (data.token) {
+      localStorage.setItem('token', data.token);
+    }
+
+    return data;
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    throw error;
+  }
 }
 
 
