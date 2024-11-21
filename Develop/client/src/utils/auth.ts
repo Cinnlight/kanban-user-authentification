ca9334fe892b2ec3db6cbe65c529280c03871f53
Cinnlight <cinnlight@gmail.com>
2024-11-21 14:23:48 -0700

    Complete logic for AuthService class, which includes authorization logic

--- Develop/client/src/utils/auth.ts
+++ Develop/client/src/utils/auth.ts
@@ -3,28 +3,47 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    if (token) {
+      return jwtDecode<JwtPayload>(token);
+    }
+    return null;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decodedToken = jwtDecode<JwtPayload>(token);
+      const currentTime = Date.now() / 1000;
+      return decodedToken.exp ? decodedToken.exp < currentTime : true;
+    } catch (error) {
+      return true;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    return localStorage.getItem('token') || '';
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('token');
     // TODO: redirect to the login page
+    window.location.assign('/login');
   }
 }
 
